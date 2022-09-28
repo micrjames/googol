beb2ba72b513fd2ddf6c0998d4b77bd4567df734
Michael James <micrjames921@gmail.com>
2022-09-27 20:33:34 -0700

    got working 'open search' and 'trigger search functionality'

---
+++ src/js/incs.js
@@ -0,0 +1,17 @@
+const body = document.body;
+
+const header = body.children.namedItem("header");
+const nav = header.children.namedItem("nav");
+const menu = nav.children.namedItem("menu");
+const menuItems = menu.children;
+const searchTrigger = menuItems.namedItem("search-trigger"); 
+
+const main = body.children.namedItem("main");
+const search = main.children.namedItem("search");
+
+const searchForm = document.forms["search-form"];
+const submitBtn = searchForm.elements["submit-btn"];
+
+const searchOutput = main.children.namedItem("search-output");
+
+export { searchTrigger, search, submitBtn, searchOutput };
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-// add JavaScript code here
+import { searchTrigger, search, submitBtn, searchOutput } from "./incs.js";
+
+searchTrigger.addEventListener("click", function() {
+    search.classList.toggle("hidden");
+});
+
+submitBtn.addEventListener("click", function(event) {
+	event.preventDefault();
+    searchOutput.classList.remove("hidden");
+    const intervalID = setInterval(function() {
+	   searchOutput.classList.add("hidden");
+	}, 2500);
+});
